4274ae11e748c793ce46d16b93f5e70b59972eca
Sam N <78718829+TetraTsunami@users.noreply.github.com>
2025-11-13 22:18:52 -0600

    icon in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+	import favicon from '$lib/assets/favicon.svg';
 </script>
 
 <header
@@ -6,15 +7,10 @@
 >
 	<div class="flex items-center text-white">
 		<a class="contents" href="/">
-			<div class="size-4 text-primary">
-				<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-					<path
-						d="M42.4379 44C42.4379 44 36.0744 33.9038 41.1692 24C46.8624 12.9336 42.2078 4 42.2078 4L7.01134 4C7.01134 4 11.6577 12.932 5.96912 23.9969C0.876273 33.9029 7.27094 44 7.27094 44L42.4379 44Z"
-						fill="currentColor"
-					></path>
-				</svg>
+			<div class="size-6 text-primary">
+				<img src={favicon} alt="logo">
 			</div>
-			<h2 class="font-display pl-4 text-lg leading-tight font-bold tracking-[-0.015em] text-white hover:text-primary transition-colors">
+			<h2 class="font-display pl-2 text-lg leading-tight font-bold tracking-[-0.015em] text-white hover:text-primary transition-colors">
 				OPERATION WEAVE
 			</h2>
 		</a>
